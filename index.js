70f7e97419755acf506d8c73768e3b79b21f66a9
HansM7 <melchorhans5@gmail.com>
2023-11-05 21:40:57 -0500

    add env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRouter from "./routes/user.route.js";
 import pokemonRouter from "./routes/pokemon.route.js";
 import cors from "cors";
+import "dotenv/config.js";
 const app = express();
 
 app.use(express.json());
@@ -13,5 +14,5 @@ app.use("/api", userRouter);
 app.use("/api", pokemonRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
