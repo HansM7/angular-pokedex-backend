7bb11b0712b82c8ced7ccda5e64f064620088ccc
HansM7 <melchorhans5@gmail.com>
2023-11-05 22:00:28 -0500

    002

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 4000;
-
 app.use("/api", userRouter);
 app.use("/api", pokemonRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
